88e5b5f45ad71b16900b46b3103555cedde313e9
TiNTen <57715701+TiNTen-coder@users.noreply.github.com>
2022-04-09 20:08:32 +0300

    pre-alpha v1.05

--- server.py
+++ server.py
@@ -1,14 +1,30 @@
 from flask import Flask
-from flask import request
+from flask import request, redirect, url_for
 from base64 import *
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def start():
-    if request.method == "POST":
-        print(request.get_json())
+    if request.method == "GET":
+        json_file = request.get_json()
+        data = b64decode(json_file["data"])
+        return redirect(url_for(f'{data["first_button"]["status"]}'), 301)
+
+@app.route('/click')
+def click():
+    return 'click'
+
+
+@app.route('/double_click')
+def double_click():
+    return 'double_click'
+
+
+@app.route('/long_press')
+def double_click():
+    return 'long_press'
 
 
 if __name__ == '__main__':
