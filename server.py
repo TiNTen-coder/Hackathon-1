22566a732908ac5e5f172e4c3a813717e9ec7dff
TiNTen <57715701+TiNTen-coder@users.noreply.github.com>
2022-04-09 22:59:05 +0300

    pre-alpha v.1.12

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ from flask import Flask
 from flask import request, redirect, url_for, render_template
 from base64 import *
 import json
+from requests import get, post, delete
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/',  methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return request.get_json()
-'''    
     try:
         json_file = request.get_json()
         data = json.loads(b64decode(json_file["data"]).decode('utf-8'))
-        return redirect(url_for(f'{data["telemetry"]["firstButton"]["status"]}'), 301)
+        return redirect(url_for(f'/{data["telemetry"]["firstButton"]["status"]}'), 301)
     except:
         return render_template('start.html')
 
@@ -31,8 +31,11 @@ def double_click():
 @app.route('/long_press')
 def long_press():
     return 'long_press'
-'''
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    '''json_file = get(f'http://localhost:{port}').json()
+    data = json.loads(b64decode(json_file["data"]).decode('utf-8'))
+    redirect(url_for(f'{data["telemetry"]["firstButton"]["status"]}'), 301)'''
+    app.run(host='0.0.0.0', port=port)
